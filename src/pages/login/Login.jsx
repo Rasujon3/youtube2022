246e275ebd376e6415c18e6a494845d6c8207954
Rasujon3 <rasujon3@gmail.com>
2022-11-08 23:28:23 +0600

    login page design done

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,7 +4,26 @@ import "./login.scss";
 const Login = () => {
   return (
     <div className="login">
-      <h1>login</h1>
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Repellendus minus amet nam esse minima beatae voluptates libero
+            reiciendis ex id!
+          </p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
