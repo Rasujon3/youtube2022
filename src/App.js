1f9724008d77c0555e3b8c23708483f1bbe05301
Rasujon3 <rasujon3@gmail.com>
2022-11-12 23:50:26 +0600

    react router dom v-6 protected

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   RouterProvider,
   Route,
   Outlet,
+  Navigate,
 } from "react-router-dom";
 import NavBar from "./components/navBar/NavBar";
 import LeftBar from "./components/leftBar/LeftBar";
@@ -13,6 +14,7 @@ import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 
 function App() {
+  const currentUser = true;
   const Layout = () => {
     return (
       <div>
@@ -25,10 +27,20 @@ function App() {
       </div>
     );
   };
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
       children: [
         {
           path: "/",
