456fe21c217c8b0d2829c4f4e7538539bf8572d9
Rasujon3 <rasujon3@gmail.com>
2022-11-12 23:43:26 +0600

    react router dom v-6 tutorial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import NavBar from "./components/navBar/NavBar";
+import LeftBar from "./components/leftBar/LeftBar";
+import RightBar from "./components/rightBar/RightBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
 function App() {
-  const layout = () => {
-    return <div></div>;
+  const Layout = () => {
+    return (
+      <div>
+        <NavBar />
+        <div>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
+      </div>
+    );
   };
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
     {
       path: "/login",
       element: <Login />,
---
+++ src/components/rightBar/RightBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./rightBar.scss";
+
+const RightBar = () => {
+  return (
+    <div>
+      <h1>RightBar</h1>
+    </div>
+  );
+};
+
+export default RightBar;
